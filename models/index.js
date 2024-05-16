cb533c3a0247d474c82eff33e93a2e6f75e8af55
bahlool-work <bahlool.ramesh70720@gmail.com>
2024-05-16 14:43:14 +0330

    add user Table

--- models/index.js
+++ models/index.js
@@ -1,2 +1,13 @@
 const Connection = require('../configs/connection.config');
 const Sequelize = require('sequelize');
+
+const db = {}
+
+db.connection = Connection
+db.op=Sequelize.Op
+db.sequelize = Sequelize
+
+db.user = require('../models/user.model')(Connection,Sequelize);
+
+
+module.exports = db
---
+++ models/user.model.js
@@ -0,0 +1,65 @@
+module.exports = (connection, sequelize) => {
+    const User = connection.define("user", {
+        userId: {
+            type: sequelize.UUID
+        },
+        userEmail: {
+            type: sequelize.STRING,
+            unique: {
+                args: true,
+                msg: 'این  نام قبلا استفاده شده است'
+            }
+            // allowNull: false,
+        },
+        userJobTitle: {
+            type: sequelize.STRING
+        },
+        userBio: {
+            type: sequelize.TEXT
+        },
+        userDefaultLanguage: {
+            type: sequelize.STRING
+        },
+        userIsActive: {
+            type: sequelize.TINYINT
+        },
+        userFirstName: {
+            type: sequelize.STRING
+        },
+        userLastName: {
+            type: sequelize.STRING
+        },
+        userLocation: {
+            type: sequelize.STRING
+        },
+        userName: {
+            type: sequelize.STRING
+        },
+        userToken: {
+            type: sequelize.STRING
+        },
+        userSaltToken: {
+            type: sequelize.STRING
+        },
+        userLastLoginIp: {
+            type: sequelize.STRING
+        },
+        userLastLoginTime: {
+            type: sequelize.STRING
+        },
+        userOtherToken: {
+            type: sequelize.TEXT
+        },
+        userPassword: {
+            type: sequelize.STRING
+        },
+        userCompany: {
+            type: sequelize.STRING
+        },
+        userProfile: {
+            type: sequelize.TEXT
+        }
+    }
+    )
+    return User
+}
